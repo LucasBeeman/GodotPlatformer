67d4535a092984a911a517b8384b95dc09bb44f7
Lucas Beeman <70167892+LucasBeeman@users.noreply.github.com>
2022-04-11 19:43:25 -0400

    p3

--- Character Scenes/Player.gd
+++ Character Scenes/Player.gd
@@ -3,8 +3,9 @@ extends KinematicBody2D
 const UP = Vector2(0, -1)
 const GRAVITY = 40
 const MAXFALLSPEED = 500
-const MAXSPEED = 200
+const MAXSPEED = 300
 const JUMPFORCE = 600
+const ACCELERATION = 30
 
 var motion = Vector2()
 
@@ -12,13 +13,15 @@ func _physics_process(delta):
 	motion.y += GRAVITY
 	if motion.y > MAXFALLSPEED:
 		motion.y = MAXFALLSPEED
+		
+	motion.x = clamp(motion.x, -MAXSPEED, MAXSPEED)
 	
 	if Input.is_action_pressed("right"):
-		motion.x = MAXSPEED
+		motion.x += ACCELERATION
 	elif Input.is_action_pressed("left"):
-		motion.x = -MAXSPEED
+		motion.x -= ACCELERATION
 	else:
-		motion.x = 0
+		motion.x = lerp(motion.x, 0, 0.2)
 		
 	if is_on_floor():
 		if Input.is_action_just_pressed("up"):
