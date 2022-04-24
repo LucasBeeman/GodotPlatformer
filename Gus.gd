c9d4dc6d7e90f352db1c930a6b0ab25173b6772e
Lucas Beeman <Lucasbeeman12@gmail.com>
2022-04-24 14:01:42 -0400

    aowekfjaov

--- Gus.gd
+++ Gus.gd
@@ -1,7 +1,7 @@
 extends RigidBody2D
 
 func _on_HurtArea_area_entered(_area):
-	get_tree().change_scene("res://LevelTwo.tscn")
+	get_tree().change_scene("res://WinScreen.tscn")
 
 func _on_HitArea_area_entered(_area):
 	get_tree().change_scene("res://LevelOne.tscn")
--- LevelOne.gd
+++ LevelOne.gd
@@ -2,3 +2,7 @@ extends Node2D
 
 func _on_Area2D_body_entered(_body):
 	 get_tree().change_scene("res://Level2.tscn")
+
+
+func _on_Area2D_area_entered(area):
+	pass # Replace with function body.
